1278e0b4d1d9b4d8b47f82a3b1323ce2d6199b16
Shubham Patel <shubhampatel196@gmail.com>
2023-04-07 17:53:53 +0530

    npm install

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const KJUR = require('jsrsasign')
 
 const app = express()
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json(), cors())
 app.options('*', cors())
